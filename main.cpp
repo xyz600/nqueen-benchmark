1d5f7a7b58cc51000658761b1ec24df8b6e2036b
xyz600 <xyz600600@gmail.com>
2021-01-03 23:16:53 +0900

    to openmp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
+#include <algorithm>
 #include <chrono>
 #include <cstdint>
 #include <iostream>
+#include <numeric>
+#include <vector>
 
 struct State
 {
@@ -48,14 +51,24 @@ std::uint64_t solve(State& state, const std::size_t n)
 
 std::uint64_t cpu_solve(const std::size_t n)
 {
-    State state;
-    state.clear();
-    return solve(state, n);
+    std::vector<std::size_t> counter(n, 0);
+#pragma omp parallel for
+    for (std::size_t column = 0; column < n; column++)
+    {
+        State state;
+        state.clear();
+        state.column_bitmap |= 1u << column;
+        state.upper_left_bitmap |= 1ull << (n - 1 + column);
+        state.upper_right_bitmap |= 1ull << column;
+        state.row++;
+        counter[column] = solve(state, n);
+    }
+    return std::accumulate(counter.begin(), counter.end(), 0);
 }
 
 int main()
 {
-    for (std::size_t n = 8; n <= 15; n++)
+    for (std::size_t n = 8; n <= 17; n++)
     {
         const auto start = std::chrono::system_clock::now();
         const auto count = cpu_solve(n);
