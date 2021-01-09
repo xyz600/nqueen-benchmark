1a13450d25be370028842ad5b0569e84daeb7167
xyz600 <xyz600600@gmail.com>
2021-01-09 17:41:52 +0900

    make cpu solver faster

--- cpu_solver.cpp
+++ cpu_solver.cpp
@@ -10,10 +10,10 @@
 
 struct State
 {
-    std::uint64_t row;
-    std::uint64_t column_bitmap;
-    std::uint64_t upper_left_bitmap;
-    std::uint64_t upper_right_bitmap;
+    std::uint32_t row;
+    std::uint32_t column_bitmap;
+    std::uint32_t upper_left_bitmap;
+    std::uint32_t upper_right_bitmap;
 
     void clear()
     {
@@ -22,9 +22,29 @@ struct State
         upper_left_bitmap = 0;
         upper_right_bitmap = 0;
     }
+    void push(std::uint32_t column_bit, const State& prev)
+    {
+        row = prev.row + 1;
+        column_bitmap = prev.column_bitmap | column_bit;
+        upper_left_bitmap = (prev.upper_left_bitmap | column_bit) >> 1;
+        upper_right_bitmap = (prev.upper_right_bitmap | column_bit) << 1;
+    }
+
+    void push_self(std::uint32_t column_bit)
+    {
+        row++;
+        column_bitmap |= column_bit;
+        upper_left_bitmap = (upper_left_bitmap | column_bit) >> 1;
+        upper_right_bitmap = (upper_right_bitmap | column_bit) << 1;
+    }
+
+    bool can_push(std::uint32_t column_bit)
+    {
+        return ((column_bitmap | upper_left_bitmap | upper_right_bitmap) & column_bit) == 0;
+    }
 };
 
-std::uint64_t solve(State& state, const std::size_t n)
+std::uint64_t solve(const State& state, const std::size_t n)
 {
     if (state.row == n)
     {
@@ -32,24 +52,14 @@ std::uint64_t solve(State& state, const std::size_t n)
     }
     std::uint64_t counter = 0;
 
-    auto bitmask = ~((state.column_bitmap | (state.upper_left_bitmap >> (n - 1 - state.row)) | (state.upper_right_bitmap >> state.row))) & ((1ull << n) - 1ull);
+    auto bitmask = ~(state.column_bitmap | state.upper_left_bitmap | state.upper_right_bitmap) & ((1ull << n) - 1ull);
     while (bitmask > 0)
     {
         const auto least_mask = -bitmask & bitmask;
+        State next;
+        next.push(least_mask, state);
 
-        const auto column_bit = least_mask;
-        const auto upper_left_bit = least_mask << (n - 1 - state.row);
-        const auto upper_right_bit = least_mask << state.row;
-
-        state.column_bitmap ^= column_bit;
-        state.upper_left_bitmap ^= upper_left_bit;
-        state.upper_right_bitmap ^= upper_right_bit;
-        state.row++;
-        counter += solve(state, n);
-        state.column_bitmap ^= column_bit;
-        state.upper_left_bitmap ^= upper_left_bit;
-        state.upper_right_bitmap ^= upper_right_bit;
-        state.row--;
+        counter += solve(next, n);
 
         bitmask -= least_mask;
     }
@@ -60,17 +70,20 @@ std::uint64_t cpu_solve(const std::size_t n)
 {
     std::vector<std::tuple<int, int, int>> c1c2;
     const std::size_t col1_max = n / 2 + (n % 2 == 0 ? 0 : 1);
-    for (std::size_t col1 = 0; col1 < col1_max; col1++)
+    for (int col1 = 0; col1 < col1_max; col1++)
     {
-        for (std::size_t col2 = 0; col2 < n; col2++)
+        for (int col2 = 0; col2 < n; col2++)
         {
-            if (n % 2 == 0)
+            if (abs(col1 - col2) > 1)
             {
-                c1c2.emplace_back(col1, col2, 2);
-            }
-            else
-            {
-                c1c2.emplace_back(col1, col2, col1 == col1_max - 1 ? 1 : 2);
+                if (n % 2 == 0)
+                {
+                    c1c2.emplace_back(col1, col2, 2);
+                }
+                else
+                {
+                    c1c2.emplace_back(col1, col2, col1 == col1_max - 1 ? 1 : 2);
+                }
             }
         }
     }
@@ -82,22 +95,10 @@ std::uint64_t cpu_solve(const std::size_t n)
         state.clear();
         const auto [column1, column2, rate] = c1c2[i];
 
-        state.column_bitmap |= 1u << column1;
-        state.upper_left_bitmap |= 1ull << (n - 1 + column1);
-        state.upper_right_bitmap |= 1ull << column1;
-        state.row += 1;
+        state.push_self(1u << column1);
+        state.push_self(1u << column2);
 
-        const std::uint32_t column_bit = 1u << column2;
-        const std::uint64_t upper_left_bit = 1ull << (n - 1 - state.row + column2);
-        const std::uint64_t upper_right_bit = 1ull << (state.row + column2);
-        if (((state.column_bitmap & column_bit) == 0u) && ((state.upper_left_bitmap & upper_left_bit) == 0ull) && ((state.upper_right_bitmap & upper_right_bit) == 0ull))
-        {
-            state.column_bitmap |= column_bit;
-            state.upper_left_bitmap |= upper_left_bit;
-            state.upper_right_bitmap |= upper_right_bit;
-            state.row++;
-            counter[i] = solve(state, n) * rate;
-        }
+        counter[i] = solve(state, n) * rate;
     }
     return std::accumulate(counter.begin(), counter.end(), 0);
 }
